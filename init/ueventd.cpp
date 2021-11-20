d2f9a0f04ef8fd8f814464a1c608a605a31b65c5
Alexander Koskovich <zvnexus@outlook.com>
2021-11-20 12:23:37 -0600

    rootdir: Look for ODM & vendor ueventd entries in ETC too.
     * This is where they should ideally go, root isn't
       preferred.
    
     * Leave the old imports from root for compatibility
       reasons.
    
    Change-Id: I3777bac327434bb2bc16f0a9f973f0f9a2291e9e

--- init/ueventd.cpp
+++ init/ueventd.cpp
@@ -287,8 +287,10 @@ int ueventd_main(int argc, char** argv) {
     // allow it to override everything.
     auto hardware = android::base::GetProperty("ro.hardware", "");
 
-    auto ueventd_configuration = ParseConfig({"/system/etc/ueventd.rc", "/vendor/ueventd.rc",
-                                              "/odm/ueventd.rc", "/ueventd." + hardware + ".rc"});
+    auto ueventd_configuration =
+            ParseConfig({"/system/etc/ueventd.rc", "/vendor/ueventd.rc", "/odm/ueventd.rc",
+                         "/ueventd." + hardware + ".rc", "/vendor/etc/ueventd." + hardware + ".rc",
+                         "/vendor/etc/ueventd.rc", "/odm/etc/ueventd.rc"});
 
     uevent_handlers.emplace_back(std::make_unique<DeviceHandler>(
             std::move(ueventd_configuration.dev_permissions),
