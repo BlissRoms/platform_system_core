a39be5324d4730deaffb3c617d9c3a3378f5eb69
Jackeagle <jackeagle102@gmail.com>
2022-10-18 12:30:02 +0200

    Revert "fastbootd: always return unlocked in GetDeviceLockStatus"
    This reverts commit 5bcf21ff9e5faf5aa0f65482c8b3930c48c0f8ae.

--- fastboot/device/utility.cpp
+++ fastboot/device/utility.cpp
@@ -196,13 +196,12 @@ std::vector<std::string> ListPartitions(FastbootDevice* device) {
 }
 
 bool GetDeviceLockStatus() {
-    //std::string cmdline;
+    std::string cmdline;
     // Return lock status true if unable to read kernel command line.
-    //if (!android::base::ReadFileToString("/proc/cmdline", &cmdline)) {
-    //    return true;
-    //}
-    //return cmdline.find("androidboot.verifiedbootstate=orange") == std::string::npos;
-    return false;
+    if (!android::base::ReadFileToString("/proc/cmdline", &cmdline)) {
+        return true;
+    }
+    return cmdline.find("androidboot.verifiedbootstate=orange") == std::string::npos;
 }
 
 bool UpdateAllPartitionMetadata(FastbootDevice* device, const std::string& super_name,
